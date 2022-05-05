6ad5c2e38fe362dbac6306ca07e2ae515817dea5
alexd <alexdidenko7@gmail.com>
2022-05-05 03:05:01 +0300

    Auto authorization fixed

--- app/src/main/java/com/dzco/myapplication/MainActivity.java
+++ app/src/main/java/com/dzco/myapplication/MainActivity.java
@@ -31,82 +31,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         root = findViewById(R.id.main_activity);
-        DatabaseReference loadProfileData = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-        InProgress progressDialog = new InProgress(MainActivity.this);
-        progressDialog.show();
-        Thread th = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                loadProfileData.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
-                    @Override
-                    public void onSuccess(DataSnapshot dataSnapshot) {
-                        user = dataSnapshot.getValue(User.class);
-                        progressDialog.hide();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        if (savedInstanceState != null) {
-                            user = (User) savedInstanceState.getSerializable("user");
-                        }
-                        progressDialog.hide();
-                        Snackbar.make(root, R.string.failed_to_load_data, Snackbar.LENGTH_SHORT).show();
-                    }
-                });
-                if (savedInstanceState == null) {
-                    bottomNavigation.setSelectedItemId(R.id.nav_profile);
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, FragmentProfile.newInstance()).commit();
-                } else {
-                    user = (User) savedInstanceState.getSerializable("user");
-                    savedFragment = savedInstanceState.getInt("selected_fragment");
-                    Fragment selectedFragment = null;
-                    bottomNavigation.setSelectedItemId(savedFragment);
-
-                    switch (savedFragment) {
-                        case R.id.nav_profile:
-                            selectedFragment = FragmentProfile.newInstance();
-                            break;
-                        case R.id.nav_news:
-                            selectedFragment = FragmentNews.newInstance();
-                            break;
-                        case R.id.nav_settings:
-                            selectedFragment = FragmentSettings.newInstance();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-                }
-
-            }
-        });
-        th.start();
         savedFragment = R.id.nav_profile;
         bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(bottomNavigationListener);
+        DatabaseReference loadProfileData = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        InProgress progressDialog = new InProgress(MainActivity.this);
 
+        progressDialog.show();
+        loadProfileData.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+                transit(savedInstanceState);
+                progressDialog.hide();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.hide();
+                transit(savedInstanceState);
+                Snackbar.make(root, R.string.failed_to_load_data, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    public void transit(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            bottomNavigation.setSelectedItemId(R.id.nav_profile);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, FragmentProfile.newInstance()).commit();
+        } else {
+            savedFragment = savedInstanceState.getInt("selected_fragment");
+            Fragment selectedFragment = null;
+            bottomNavigation.setSelectedItemId(savedFragment);
+
+            switch (savedFragment) {
+                case R.id.nav_profile:
+                    selectedFragment = FragmentProfile.newInstance();
+                    break;
+                case R.id.nav_news:
+                    selectedFragment = FragmentNews.newInstance();
+                    break;
+                case R.id.nav_settings:
+                    selectedFragment = FragmentSettings.newInstance();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable("user", User.class);
         outState.putInt("selected_fragment", bottomNavigation.getSelectedItemId());
-        outState.putString("name", user.getName());
-        outState.putBoolean("sex", user.isSex());
-        outState.putInt("age", user.getAge());
-        outState.putBoolean("chronicDiseases", user.isChronicDiseases());
-        outState.putBoolean("badHabits", user.isBadHabits());
-        outState.putString("measure", user.getMeasure());
-        outState.putString("vaccine", user.getVaccine());
-        outState.putInt("day1", user.getFirst().getDay());
-        outState.putInt("month1", user.getFirst().getMonth());
-        outState.putInt("year1", user.getFirst().getYear());
-        outState.putInt("day2", user.getSecond().getDay());
-        outState.putInt("month2", user.getSecond().getMonth());
-        outState.putInt("year2", user.getSecond().getYear());
-        outState.putString("lifePlace", user.getLifePlace());
-        outState.putString("email", user.getEmail());
-        outState.putString("password", user.getPassword());
-        outState.putString("imageURI", user.getImageURI());
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationListener =
