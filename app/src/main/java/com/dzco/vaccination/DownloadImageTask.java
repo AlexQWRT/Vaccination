6d029a40bc433fdc6d4938fe150995d0509f9399
alexqwrt <alexdidenko7@gmail.com>
2022-05-07 14:57:35 +0300

    onPreExecute() method added on image download task

--- app/src/main/java/com/dzco/vaccination/DownloadImageTask.java
+++ app/src/main/java/com/dzco/vaccination/DownloadImageTask.java
@@ -1,17 +1,17 @@
 package com.dzco.vaccination;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
 import java.io.InputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import pl.droidsonroids.gif.GifImageView;
-import  com.dzco.vaccination.R;
 
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     private final CircleImageView accountImage;
@@ -24,17 +24,20 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         this.activity = activity;
     }
 
-    protected Bitmap doInBackground(String... urls) {
+    protected void onPreExecute() {
         loadingView.setVisibility(View.VISIBLE);
+    }
+
+    protected Bitmap doInBackground(@NonNull String... urls) {
         String url = urls[0];
-        Bitmap mIcon11 = null;
+        Bitmap image = null;
         try {
             InputStream in = new java.net.URL(url).openStream();
-            mIcon11 = BitmapFactory.decodeStream(in);
+            image = BitmapFactory.decodeStream(in);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return mIcon11;
+        return image;
     }
 
     protected void onPostExecute(Bitmap result) {
